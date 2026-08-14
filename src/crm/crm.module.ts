8e12645bc91be0797323fa782ef57ab28e45b3d1
Bauyrzhan01 <Bauyrzhan01@users.noreply.github.com>
2026-08-14 17:19:32 +0500

    Fix CrmModule: import CompaniesModule for CrmController DI.

--- src/crm/crm.module.ts
+++ src/crm/crm.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LeadCrmService } from './lead-crm.service';
 import { CrmController } from './crm.controller';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { CompaniesModule } from '../companies/companies.module';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [NotificationsModule, CompaniesModule],
   controllers: [CrmController],
   providers: [LeadCrmService],
   exports: [LeadCrmService],
